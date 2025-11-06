7a35ff781eef5a9d8196e05ed8db6542594ab5d4
Rodrigo <razevedo.contato@gmail.com>
2025-11-06 10:36:06 -0300

    infra: Define creation of s3 buckets

---
+++ infra/s3.tf
@@ -0,0 +1,43 @@
+locals {
+  bucket_names = toset(["raw", "silver", "gold"])
+}
+
+
+resource "aws_s3_bucket" "lakehouse_zones" {
+    for_each = local.bucket_names
+
+    bucket = each.key
+
+    tags = {
+        Name = "Bucket ${each.key}"
+    }
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+    for_each = aws_s3_bucket.lakehouse_zones
+
+    bucket = each.value.id
+
+    rule {
+        object_ownership = "BucketOwnerPreferred"
+    }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.this]
+
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+    for_each = aws_s3_bucket.lakehouse_zones
+
+    bucket = each.value.id
+
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+}
