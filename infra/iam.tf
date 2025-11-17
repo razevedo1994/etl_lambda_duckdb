d84aeb67dc3b07aae0aa237f1359edd965e34e05
Rodrigo <razevedo.contato@gmail.com>
2025-11-17 18:04:47 -0300

    infra: Create lambda policy

---
+++ infra/iam.tf
@@ -0,0 +1,79 @@
+data "aws_iam_policy_document" "assume_role" {
+    statement {
+      effect = "Allow"
+
+      principals {
+        type        = "Service"
+        identifiers = ["lambda.amazonaws.com"]
+      }
+
+      actions = ["sts:AssumeRole"]
+    }
+}
+
+resource "aws_iam_role" "role" {
+    name                = "role_lambda_duckdb"
+    assume_role_policy  = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "policy_document" {
+  statement {
+    sid       = "AllowS3Access"
+    effect    = "Allow"
+    actions   = [
+      "s3:PutObject",
+      "s3:GetObject",
+    ]
+    resources = [
+      "arn:aws:s3:::poc-lambda-duckdb/*",
+    ]
+  }
+
+  statement {
+    sid       = "AllowS3ListAccess"
+    effect    = "Allow"
+    actions   = [
+      "s3:ListBucket"
+    ]
+    resources = [
+      "arn:aws:s3:::poc-lambda-duckdb",
+    ]
+  }
+
+  statement {
+    sid       = "AllowEC2Access"
+    effect    = "Allow"
+    actions   = [
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface"
+    ]
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    sid       = "AllowCloudWatchLogsAccess"
+    effect    = "Allow"
+    actions   = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = [
+      "arn:aws:logs:*:*:*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "policy_lambda_duckdb"
+  description = "Policy for lambda function"
+  policy      = data.aws_iam_policy_document.policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
