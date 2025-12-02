9aeab23d764d49462f2bce270fefdff7d8fa0efe
Rodrigo <razevedo.contato@gmail.com>
2025-12-02 14:06:38 -0300

    infra: Create resource for lambda layer

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,31 +1,29 @@
 data "archive_file" "lambda" {
   type          = "zip"
   source_file   = "./app/main.py"
-  output_path   = "./lambda_layer/lambda_function_payload.zip"
+  output_path   = "./lambda_code/lambda_function_payload.zip"
+}
+
+resource "aws_lambda_layer_version" "my_python_layer" {
+  layer_name          = "my-python-dependencies"
+  filename            = "./app/python.zip"
+  compatible_runtimes = ["python3.10"]
+  description         = "Layer with common Python libraries"
 }
 
 resource "aws_lambda_function" "this" {
-  filename      = "./lambda_layer/lambda_function_payload.zip"
+  filename      = "./lambda_code/lambda_function_payload.zip"
   function_name = var.lambda_name
   handler       = "main.lambda_handler"
   role          = aws_iam_role.role.arn
 
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  source_code_hash = filebase64sha256("./lambda_code/lambda_function_payload.zip")
 
-  runtime = "python3.12"
+  layers = [aws_lambda_layer_version.my_python_layer.arn]
+
+  runtime = "python3.10"
 
   depends_on = [ 
     aws_iam_role_policy_attachment.attach
    ]
-
-#   vpc_config {
-#     subnet_ids            = [aws_subnet.this.id]
-#     security_group_ids  = [aws_security_group.this.id] 
-#   }
-
-#   environment {
-#     variables = {
-#       S3_BUCKET = aws_s3_bucket.this.bucket
-#     }
-#   }
 }
