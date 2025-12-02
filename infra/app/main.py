dc82d422cd8cfca5d5ec7b2c414c42d0caa22cbc
Rodrigo <razevedo.contato@gmail.com>
2025-12-02 14:07:39 -0300

    feat: Update function

--- infra/app/main.py
+++ infra/app/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from app!")
+import duckdb
+import os
+import boto3
 
 
-if __name__ == "__main__":
-    main()
+KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+SECRET_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+
+def lambda_handler(event, context):
+    print(event)
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = event["Records"][0]["s3"]["object"]["key"]
+
+    conn = duckdb.connect()
+
+    conn.query(
+        """
+               INSTALL httpfs;
+               LOAD httpfs;
+                CREATE SECRET secretaws (
+                TYPE S3,
+                PROVIDER CREDENTIAL_CHAIN
+            );
+               """
+    )
