5fa31cc40ef00bc87dce9f75e597c4304f698749
Rodrigo <razevedo.contato@gmail.com>
2025-12-04 08:28:17 -0300

    infra: Update lambda resource to use an image from ECR

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,18 +1,18 @@
-data "archive_file" "lambda" {
-  type          = "zip"
-  source_file   = "./app/main.py"
-  output_path   = "./lambda_code/lambda_function_payload.zip"
+data "aws_ecr_repository" "duckdb_image" {
+  name = "lambda_duckdb"
 }
 
 resource "aws_lambda_function" "this" {
-  filename      = "./lambda_code/lambda_function_payload.zip"
+  # filename      = "./lambda_code/lambda_function_payload.zip"
+  # handler       = "main.lambda_handler"
   function_name = var.lambda_name
-  handler       = "main.lambda_handler"
+  package_type = "Image"
+  image_uri     = "${data.aws_ecr_repository.duckdb_image.repository_url}/lambda:latest"
   role          = aws_iam_role.role.arn
 
-  source_code_hash = filebase64sha256("./lambda_code/lambda_function_payload.zip")
+  # source_code_hash = filebase64sha256("./lambda_code/lambda_function_payload.zip")
 
-  runtime = "python3.12"
+  # runtime = "python3.12"
 
   depends_on = [ 
     aws_iam_role_policy_attachment.attach
