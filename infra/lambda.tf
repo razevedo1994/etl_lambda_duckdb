9dc70ef6170c6206489ac9c4ad3e4afadc446bbd
Rodrigo <razevedo.contato@gmail.com>
2025-12-09 19:54:44 -0300

    infra: Update lambda resource to use ECR image

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,20 +1,11 @@
-data "aws_ecr_repository" "duckdb_image" {
-  name = "lambda_duckdb"
-}
-
 resource "aws_lambda_function" "this" {
-  # filename      = "./lambda_code/lambda_function_payload.zip"
-  # handler       = "main.lambda_handler"
   function_name = var.lambda_name
-  package_type = "Image"
-  image_uri     = "${data.aws_ecr_repository.duckdb_image.repository_url}/lambda:latest"
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.lambda_duckdb.repository_url}:latest"
   role          = aws_iam_role.role.arn
 
-  # source_code_hash = filebase64sha256("./lambda_code/lambda_function_payload.zip")
-
-  # runtime = "python3.12"
-
-  depends_on = [ 
-    aws_iam_role_policy_attachment.attach
-   ]
+  depends_on = [
+    aws_iam_role_policy_attachment.attach,
+    terraform_data.docker_build
+  ]
 }
